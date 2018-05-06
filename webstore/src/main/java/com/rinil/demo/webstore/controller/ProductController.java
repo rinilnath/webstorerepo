bd7ab2c310116eca1a10de488d3b9c214cbb47f4
rinilnath <rinilnath@gmail.com>
2018-05-06 14:41:25 +0530

    Morechanges
    Enhanced changes in webstore project

--- webstore/src/main/java/com/rinil/demo/webstore/controller/ProductController.java
+++ webstore/src/main/java/com/rinil/demo/webstore/controller/ProductController.java
@@ -7,26 +7,23 @@ package com.rinil.demo.webstore.controller;
  * @author rinilnath
  *
  */
-import java.math.BigDecimal;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.rinil.demo.webstore.domain.Product;
+import com.rinil.demo.webstore.domain.repository.ProductRepository;
 
 @Controller
 public class ProductController {
 
+	@Autowired
+	private ProductRepository productRepository;
+
 	@RequestMapping("/products")
 	public String list(Model model) {
-		Product iphone = new Product("P1234", "iPhone 6s", new BigDecimal(500));
-		iphone.setDescription("Apple iPhone 6s smartphone with 4.00-inch 640x1136 display and 8-megapixel rear camera");
-		iphone.setCategory("Smartphone");
-		iphone.setManufacturer("Apple");
-		iphone.setUnitsInStock(1000);
 
-		model.addAttribute("product", iphone);
+		model.addAttribute("product", productRepository.getAllProducts());
 
 		return "products";
 	}
---
+++ webstore/src/main/java/com/rinil/demo/webstore/domain/repository/ProductRepository.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.rinil.demo.webstore.domain.repository;
+
+/**
+ * @author rinilnath
+ *
+ */
+
+import java.util.List;
+
+import com.rinil.demo.webstore.domain.Product;
+
+public interface ProductRepository {
+
+	List<Product> getAllProducts();
+}
